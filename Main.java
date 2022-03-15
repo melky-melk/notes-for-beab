f7786b774a617354f641dd2e1754baf07033b9bd
melky-melk <chiara.justine.tan@gmail.com>
2022-03-15 12:39:27 +1100

    adding private and public attributes

--- Main.java
+++ Main.java
@@ -1,31 +1,46 @@
 // when you make a class its like you have a blueprint on how it will be made
 class Person{
 	// these are called attributes, like the information a class can contain in
-	// so a Person will have a name and an age
-	String name;
-	int age;
+	// so a Person will have a onlineName and an age
+	public String onlineName;
+	public int age;
+
+	// private meaning that it can only be accessed and changed from within its own class
+	private String realName; 
 
 	// this is a constructor, how you make "objects" from a class
-	public Person(String name, int age){
-		this.name = name;
+	public Person(String onlineName, int age, String realName){
+		this.onlineName = onlineName;
 		this.age = age;
+		this.realName = realName;
 	} 
 
 	// objects can also have methods inside of it
 	public void printPerson(){
-		System.out.println(this.name + " is " + this.age + " years old");
+		System.out.println(this.onlineName + " is " + this.age + " years old");
 	}
+
+	// returns the real name
+	public String getRealName() {return this.realName;}
+
+	// given a new name, it will change the realName
+	public void setRealName(String newName) {this.realName = newName;}
 }
 
 public class Main {
 
 	public static void main(String[] args){
 		// objects are created like this, its like how you make a scanner
-		Person melk = new Person("Chiara", 19);
-		System.out.println(melk.name);
+		Person melk = new Person("melk", 19, "Chiara");
+		System.out.println(melk.onlineName);
+		System.out.println(melk.getRealName());
+
+		// changes the real name
+		melk.setRealName("Chickadee");
+		System.out.println(melk.getRealName());
 
 		
-		Person beab = new Person("Bean", 18);
+		Person beab = new Person("Bean", 18, "Emily");
 		beab.printPerson();
 	}
 }
